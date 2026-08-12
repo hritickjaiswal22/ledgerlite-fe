2f2f199efa897fe1f27cd04b04c0aae1ba8a5cb3
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2026-08-12 18:40:42 +0530

    feat accounts v4

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -3,7 +3,7 @@ import NavLinks from "@/components/layouts/nav";
 
 function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 border-b border-border bg-header-bg backdrop-blur-md">
+    <header className="fixed z-10 top-0 left-0 right-0 border-b border-border bg-header-bg backdrop-blur-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-4 h-16">
         <div className="flex items-center gap-8">
           <span className="text-primary text-headline-lg font-bold">
